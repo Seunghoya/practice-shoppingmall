332b5bd027430f676d7a195187027756e2cc972d
Seunghoya <viva.primavera121@gmail.com>
2022-05-31 13:03:03 +0900

    Router 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,20 @@
-
-import React from 'react';
 import GlobalLayout from './pages/_layout'
-
-const DynamicIndex = React.lazy(() => import('./pages/index'));
-const DynamicCartIndex = React.lazy(() => import('./pages/cart/index'));
-const DynamicGnb = React.lazy(() => import('./pages/gnb'));
-const DynamicProductsIndex = React.lazy(() => import('./pages/products/index'));
-const DynamicProductsId = React.lazy(() => import('./pages/products/[id]'));
-
+import Index from './pages/index';
+import CartIndex from './pages/cart/index';
+import Gnb from './pages/gnb';
+import ProductsIndex from './pages/products/index';
+import ProductsId from './pages/products/[id]';
 
 export const routes = [
   {
     path: '/',
     element: <GlobalLayout />,
     children: [
-      { path: '/', element: <DynamicIndex />, index: true},
-      { path: '/cart', element: <DynamicCartIndex />, index: true},
-      { path: '/gnb', element: <DynamicGnb />, },
-      { path: '/products', element: <DynamicProductsIndex />, index: true},
-      { path: '/products/:id', element: <DynamicProductsId />, },
+      { path: '/', element: <Index />, index: true},
+      { path: '/cart', element: <CartIndex />, index: true},
+      { path: '/gnb', element: <Gnb />, },
+      { path: '/products', element: <ProductsIndex />, index: true},
+      { path: '/products/:id', element: <ProductsId />, },
     ]
   }
 ]
