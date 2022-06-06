3c8e532d497ad740f96eb7b973fabff256b3bcf6
Seunghoya <viva.primavera121@gmail.com>
2022-06-06 21:03:01 +0900

    장바구니 수량 업데이트 setQuery 수정

--- src/components/cart/item.tsx
+++ src/components/cart/item.tsx
@@ -16,11 +16,22 @@ export const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
     updateCart(
       { id, amount },
       {
-        onSuccess: () => queryClient.invalidateQueries(QueryKeys.CART)
+        // onSuccess: () => queryClient.invalidateQueries(QueryKeys.CART)
+        // 해당 방법은 Req를 두 번씩 보내는 단점이 존재함. 자주 요청을 보내는 것이 좋은것인지 고민해볼때
+        // 캐시에 저장해서 반영하는 방법으로 요청을 최소화 하는것이 최적화에 더 좋아보임.
+        
+        onSuccess: (newValue) => {
+          const prevCart =  queryClient.getQueryData<{[key: string]: CartType}>(QueryKeys.CART)
+          const newCart = {
+            ...(prevCart || {}),
+            ...newValue
+          }
+          queryClient.setQueryData(QueryKeys.CART, newCart)
+        }
       }
-      )
+    )
   }
-  
+
   return (
     <ul>
       <h3>{title}</h3>
