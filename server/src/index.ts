b2f11fafd4073c8e4659091e4b963588f0c5ecc7
Seunghoya <viva.primavera121@gmail.com>
2022-06-14 17:50:08 +0900

    cors 도메인 apollographql 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { ApolloServer, ExpressContext } from "apollo-server-express"
 import { Config } from 'apollo-server-core';
+import schema from "../schema";
+import resolvers from "../resolvers";
 
 
 const port = 8000;
 
 (async () => {
-  const server = new ApolloServer({} as Config<ExpressContext>)
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    context: {}
+
+  })
 
   const app = express()
   await server.start()
@@ -14,7 +21,7 @@ const port = 8000;
     app,
     path: '/graphql',
     cors: {
-      origin: ['http://localhost:3000'],
+      origin: ['http://localhost:3000', 'https://studio.apollographql.com/sandbox'],
       credentials: true
     }
   })
